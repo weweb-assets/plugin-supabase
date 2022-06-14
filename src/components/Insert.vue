4ba8158428bb49def8f3df8774b9fcfbe49c12a2
Dams-d <damien.duro8@gmail.com>
2022-06-14 16:05:51 +0200

    Action insert

--- src/components/Insert.vue
+++ src/components/Insert.vue
@@ -13,6 +13,29 @@
         </div>
         <button type="button" class="ww-editor-button -small -primary ml-2 mt-3" @click="fetchTables">refresh</button>
     </div>
+    <template v-if="table">
+        <wwEditorInputRow
+            label="Fields"
+            type="select"
+            required
+            multiple
+            :options="tablePropertiesOptions"
+            :model-value="dataFields"
+            placeholder="All data fields"
+            @update:modelValue="setDataFields"
+        />
+        <wwEditorInputRow
+            v-for="property of tablePropertiesFiltered"
+            :key="property.name"
+            :label="property.name"
+            :placeholder="`${property.default ? `Default: ${property.default}` : 'Enter a value '}`"
+            :type="property.type"
+            :required="property.required"
+            :model-value="data[property.name]"
+            @update:modeValue="update"
+            bindable
+        />
+    </template>
     <wwLoader :loading="isLoading" />
 </template>
 
@@ -33,19 +56,65 @@ export default {
         table() {
             return this.args.table;
         },
+        dataFields() {
+            return this.args.dataFields || [];
+        },
+        data() {
+            return this.args.data || {};
+        },
         tablesOptions() {
             return Object.keys(this.definitions).map(tableName => ({
                 label: tableName,
                 value: tableName,
             }));
         },
+        tableProperties() {
+            if (!this.definitions[this.table]) return [];
+            return Object.keys(this.definitions[this.table].properties).map(propertyName => ({
+                name: propertyName,
+                type:
+                    this.definitions[this.table].properties[propertyName].type === 'string'
+                        ? 'query'
+                        : this.definitions[this.table].properties[propertyName].type,
+                required:
+                    this.definitions[this.table].required.includes(propertyName) &&
+                    !this.definitions[this.table].properties[propertyName].default,
+                default: this.definitions[this.table].properties[propertyName].default,
+            }));
+        },
+        tablePropertiesFiltered() {
+            return this.tableProperties.filter(
+                property => !this.dataFields.length || this.dataFields.includes(property.name)
+            );
+        },
+        tablePropertiesOptions() {
+            return this.tableProperties.map(property => ({
+                label: property.name,
+                value: property.name,
+            }));
+        },
     },
     mounted() {
-        this.definitions = this.plugin.doc.definitions || {};
+        this.definitions = (this.plugin.doc && this.plugin.doc.definitions) || {};
     },
     methods: {
         setTable(table) {
             this.$emit('update:args', { ...this.args, table });
+        },
+        setDataFields(dataFields) {
+            this.$emit('update:args', { ...this.args, dataFields });
+            this.$nextTick(() => this.setData({ ...this.data }));
+        },
+        setData(data) {
+            for (const dataKey in data) {
+                if (!this.tablePropertiesFiltered.find(field => field.name === dataKey)) {
+                    delete data[dataKey];
+                }
+            }
+            for (const field of this.tablePropertiesFiltered) {
+                data[field.name] = data[field.name] || null;
+            }
+            this.$emit('update:args', { ...this.args, data });
         },
         async fetchTables() {
             try {
