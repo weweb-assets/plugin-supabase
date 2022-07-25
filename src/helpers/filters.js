eedf53aaf1097d0b4021189fec784d14afe46c22
Alexis JC <alexisjc.dev@gmail.com>
2022-07-25 18:18:39 +0200

    refactor: export helpers

---
+++ src/helpers/filters.js
@@ -0,0 +1,56 @@
+export function convertCondition({field, operator, value}) {
+    switch (operator) {
+        case '$eq':
+            return [field, 'eq', value]
+        case '$ne':
+            return [field, 'neq', value]
+        case '$lt':
+            return [field, 'lt', value]
+        case '$gt':
+            return [field, 'gt', value]
+        case '$lte':
+            return [field, 'lte', value] 
+        case '$gte':
+            return [field, 'gte', value] 
+        case '$iLike:contains': // not possible on array
+            return [field, 'ilike', `%${value}%`]
+        case '$notILike:contains': // not possible on array
+            return [field, 'not.ilike', `%${value}%`]
+        case '$iLike:startsWith':
+            return [field, 'ilike', `${value}%`]
+        case '$iLike:endsWith':
+            return [field, 'ilike', `%${value}`]
+        case '$eq:null':
+            return [field, 'is', 'null']
+        case '$ne:null':
+            return [field, 'not.is', 'null']
+        case '$in':
+            return [field, 'in', `(${value})`]
+        case '$notIn':
+            return [field, 'not.in', `(${value})`]
+        case '$overlap':
+            return [field, 'ov', `{${value}}`]
+        case '$notOverlap':
+            return [field, 'not.ov', `{${value}}`]
+        case '$contains':
+            return [field, 'cs', `{${value}}`]
+        case '$has':
+            return [`${field}->${value}`, 'neq', 'null']
+        default:
+            break;
+    }
+}
+
+export function generateFilter(config) {
+    if(!config) return ''
+
+    const conditions = config.conditions.map(condition => {
+        return condition.link ? generateFilter(condition) : convertCondition(condition).join('.')
+    }).filter(condition => condition)
+    
+    if(!conditions.length) return ''
+
+    const filter = `${config.link.slice(1)}(${conditions.join()})`
+
+    return filter
+}
--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -13,64 +13,7 @@ import './components/Functions/Delete.vue';
 /* wwEditor:end */
 import { createClient } from '@supabase/supabase-js';
 
-function convertCondition({field, operator, value}) {
-    // const isArray = Array.isArray(value)
-
-    switch (operator) {
-        case '$eq':
-            return [field, 'eq', value]
-        case '$ne':
-            return [field, 'neq', value]
-        case '$lt':
-            return [field, 'lt', value]
-        case '$gt':
-            return [field, 'gt', value]
-        case '$lte':
-            return [field, 'lte', value] 
-        case '$gte':
-            return [field, 'gte', value] 
-        case '$iLike:contains': // not possible on array
-            return [field, 'ilike', `%${value}%`]
-        case '$notILike:contains': // not possible on array
-            return [field, 'not.ilike', `%${value}%`]
-        case '$iLike:startsWith':
-            return [field, 'ilike', `${value}%`]
-        case '$iLike:endsWith':
-            return [field, 'ilike', `%${value}`]
-        case '$eq:null':
-            return [field, 'is', 'null']
-        case '$ne:null':
-            return [field, 'not.is', 'null']
-        case '$in':
-            return [field, 'in', `(${value})`]
-        case '$notIn':
-            return [field, 'not.in', `(${value})`]
-        case '$overlap':
-            return [field, 'ov', `{${value}}`]
-        case '$notOverlap':
-            return [field, 'not.ov', `{${value}}`]
-        case '$contains':
-            return [field, 'cs', `{${value}}`]
-        case '$has':
-            return [`${field}->${value}`, 'neq', 'null']
-        default:
-            break;
-    }
-}
-
-function generateFilter(config) {
-    if(!config) return ''
-
-    const conditions = config.conditions.map(condition => {
-        return condition.link ? generateFilter(condition) : convertCondition(condition).join('.')
-    }).filter(condition => condition)
-    
-    if(!conditions.length) return ''
-
-    const filter = `${config.link.slice(1)}(${conditions.join()})`
-
-    return filter
-}
+import { generateFilter } from './helpers/filters'
 
 export default {
     instance: null,
@@ -111,6 +54,7 @@ export default {
                 return { data, error, total: count };
             } catch (err) {
                 return {
+                    data: [],
                     error: Object.getOwnPropertyNames(err).reduce((obj, key) => ({ ...obj, [key]: err[key] }), {}),
                 };
             }
