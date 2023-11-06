a99dbd662ec8143c23445d1402a626a0181b86dc
Alexis JC <alexisjc.dev@gmail.com>
2023-11-06 17:04:22 +0100

    fix: typo returnDataFields

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -131,7 +131,7 @@ export default {
             returnData,
             autoSync,
             returnFieldsMode,
-            returnDataField = [],
+            returnDataFields = [],
             returnDataFieldsAdvanced,
         },
         wwUtils
