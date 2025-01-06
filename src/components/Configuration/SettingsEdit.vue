d38c99f05f9ace9b9436896187c4e7ee9eb1732d
Alexis JC <alexisjc.dev@gmail.com>
2025-01-06 19:17:16 +0100

    feat: add access token

--- src/components/Configuration/SettingsEdit.vue
+++ src/components/Configuration/SettingsEdit.vue
@@ -20,6 +20,20 @@
         :model-value="settings.publicData.apiKey"
         @update:modelValue="changeApiKey"
     />
+    <wwEditorInputRow
+        label="Personal Access Token"
+        required
+        type="query"
+        placeholder="sbp_bdd0********4f23"
+        :model-value="settings.privateData.accessToken"
+        @update:modelValue="changeAccessToken"
+    >
+        <template #append-label>
+            <a class="ww-editor-link ml-2" href="https://supabase.com/dashboard/account/tokens" target="_blank">
+                Find it here
+            </a>
+        </template>
+    </wwEditorInputRow>
 </template>
 
 <script>
@@ -39,12 +53,17 @@ export default {
         if (!isSettingsValid && isOtherPluginSettingsValid) {
             this.changeProjectUrl(wwLib.wwPlugins.supabaseAuth.settings.publicData.projectUrl);
             this.changeApiKey(wwLib.wwPlugins.supabaseAuth.settings.publicData.apiKey);
+            this.changeAccessToken(wwLib.wwPlugins.supabaseAuth.settings.privateData.accessToken);
             this.$emit('update:settings', {
                 ...this.settings,
                 publicData: {
                     ...this.settings.publicData,
                     apiKey: wwLib.wwPlugins.supabaseAuth.settings.publicData.apiKey,
                     projectUrl: wwLib.wwPlugins.supabaseAuth.settings.publicData.projectUrl,
+                },
+                privateData: {
+                    ...this.settings.privateData,
+                    accessToken: wwLib.wwPlugins.supabaseAuth.settings.privateData.accessToken,
                 },
             });
         }
@@ -63,6 +82,12 @@ export default {
                 publicData: { ...this.settings.publicData, apiKey },
             });
             this.$nextTick(this.loadInstance);
+        },
+        changeAccessToken(accessToken) {
+            this.$emit('update:settings', {
+                ...this.settings,
+                privateData: { ...this.settings.privateData, accessToken },
+            });
         },
         loadInstance() {
             this.plugin.load(this.settings.publicData.projectUrl, this.settings.publicData.apiKey);
