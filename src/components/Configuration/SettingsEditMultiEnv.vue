8347502a178e785f25b7e4160eb4451262977e8d
Léo Coletta <mail.leo.coletta@gmail.com>
2025-09-29 11:17:46 +0200

    fix(WW-4009): Use latest base ref when loading branches

--- src/components/Configuration/SettingsEditMultiEnv.vue
+++ src/components/Configuration/SettingsEditMultiEnv.vue
@@ -603,17 +603,17 @@ export default {
             });
 
             // Load branches (do not gate UI on success)
-            await this.loadBranches(env);
+            await this.loadBranches(env, baseProjectRef);
         },
 
-        async loadBranches(env) {
+        async loadBranches(env, overrideRef = '') {
             try {
                 const cfg = this.getCurrentEnvConfig(env);
-                const baseRef = cfg.baseProjectRef || cfg.projectUrl?.replace('https://', '').replace('.supabase.co', '');
+                const baseRef = overrideRef || cfg.baseProjectRef || cfg.projectUrl?.replace('https://', '').replace('.supabase.co', '');
                 const ref = baseRef;
-                console.info('[Supabase plugin] loadBranches', { env, baseProjectRef: cfg.baseProjectRef, projectUrl: cfg.projectUrl, ref, hasOAuth: this.hasOAuthToken() });
+                console.info('[Supabase plugin] loadBranches', { env, baseProjectRef: cfg.baseProjectRef, overrideRef, projectUrl: cfg.projectUrl, ref, hasOAuth: this.hasOAuthToken() });
                 if (!ref || !this.hasOAuthToken()) return;
-                const { data } = await wwLib.wwPlugins.supabase.requestAPI({ method: 'GET', path: `/projects/${ref}/branches`, params: { baseProjectRef: cfg.baseProjectRef || '' } });
+                const { data } = await wwLib.wwPlugins.supabase.requestAPI({ method: 'GET', path: `/projects/${ref}/branches`, params: { baseProjectRef: cfg.baseProjectRef || overrideRef || '' } });
                 this.$set(this.branches, env, data?.data || []);
                 this.$set(this.branchErrors, env, '');
                 console.info('[Supabase plugin] loadBranches success', { env, count: data?.data?.length || 0 });
@@ -654,6 +654,8 @@ export default {
                 },
                 privateData: { apiKey: privateApiKey || this.getCurrentEnvPrivateConfig(env).apiKey, connectionString: connectionString || this.getCurrentEnvPrivateConfig(env).connectionString }
             });
+
+            await this.loadBranches(env, baseRef);
         },
         
         changeApiKey(apiKey, env) {
